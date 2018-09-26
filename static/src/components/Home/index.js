d640e17c78ff61ccab3c710b339706f645df0b04
Bill Tang <billt4ng@gmail.com>
2018-09-25 18:58:13 -0700

    initial commit

---
+++ static/src/components/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Home = () =>
+    <section>
+        <div className="container text-center">
+            <h1>Welcome to Keras Model Builder</h1>
+            <img src='https://s3.amazonaws.com/keras.io/img/keras-logo-2018-large-1200.png'/>
+        </div>
+    </section>
