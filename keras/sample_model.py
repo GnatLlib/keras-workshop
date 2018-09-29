54243bd0db55ff37efcb766532887d96bffd08ff
Bill Tang <billt4ng@gmail.com>
2018-09-28 20:42:15 -0700

    adding sample_model and fixing gitignore

---
+++ keras/sample_model.py
@@ -0,0 +1,17 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import plot_model
+import keras as keras
+
+model = Sequential()
+model.add(Dense(units=64, activation='relu', input_dim=100))
+model.add(Dense(units=10, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
+
+model_json = model.to_json()
+model_file = open("test_model.json", "w")
+model_file.write(model_json)
+
+plot_model(model, to_file='model.png', show_shapes=True)
