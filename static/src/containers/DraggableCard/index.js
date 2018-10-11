66384c342e34a8d0fdd7479980fd4f2f4b4df81d
Bill Tang <billt4ng@gmail.com>
2018-10-10 19:55:28 -0700

    adding react-grid-layout dependency and work on DraggableCard

--- static/src/containers/DraggableCard/index.js
+++ static/src/containers/DraggableCard/index.js
@@ -1,3 +1,20 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { DragSource } from "react-dnd";
+
+const cardSource = {
+    beginDrag(props) {
+        return {
+            ...props
+        };
+    }
+};
+
+@DragSource("Card", cardSource, (connect, monitor) => ({
+    connectDragSource: connect.dragSource()
+}))
+export default class DraggableCard extends Component {
+    render() {
+        return this.props.connectDragSource(<div>{this.props.children}</div>);
+    }
+}
